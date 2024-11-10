d7de41634a7b0672fdcb3477d29010a2e6e0efc9
Qiankhor <yiqiankhor0525@gmail.com>
2024-11-10 11:04:41 +0800

    Debug problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,18 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  try {
+    await Firebase.initializeApp(
       options: const FirebaseOptions(
-          apiKey: 'AIzaSyA7mUBkc2n8LBxcoLnPXNlrNF9oRfDsvFk',
-          appId: '1:865014841760:android:e427e8517526f3af0fe4c5',
-          messagingSenderId: '865014841760',
-          projectId: 'educonnect-410d6'));
+        apiKey: 'AIzaSyA7mUBkc2n8LBxcoLnPXNlrNF9oRfDsvFk',
+        appId: '1:865014841760:android:e427e8517526f3af0fe4c5',
+        messagingSenderId: '865014841760',
+        projectId: 'educonnect-410d6',
+      ),
+    );
+  } catch (e) {
+    print("Firebase initialization failed: $e");
+  }
+
   runApp(const EduConnectApp());
 }
